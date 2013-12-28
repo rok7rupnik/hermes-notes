81ddd12a30c9dac2a76cce1b6a7834ec319d2c85
rok7rupnik <rok7rupnik@gmail.com>
2013-12-28 14:39:51 +0100

    Uredil migratione za heroku

--- db/migrate/20131228125813_add_rows_to_posta.rb
+++ db/migrate/20131228125813_add_rows_to_posta.rb
@@ -1,12 +1,12 @@
 #encoding: utf-8
 class AddRowsToPosta < ActiveRecord::Migration
   def self.up
-    execute "INSERT INTO posta (postnast, naziv) VALUES
+    execute "INSERT INTO posta (`postnaSt`, `naziv`) VALUES
               (1000, 'Ljubljana'),
               (1001, 'Ljubljana - poštni predali')"
   end
 
   def self.down
-    execute "DELETE FROM posta WHERE postnaSt > 999"
+    execute "DELETE FROM posta WHERE `postnaSt` > 999"
   end
 end
